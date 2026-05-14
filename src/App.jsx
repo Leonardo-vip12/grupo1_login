8cd1d9ec10d92cc680d4ef40203d954bf02ce7df
FRANCISCO DAVID PEZO TORREZ <pezotorresfranciscodavid2@gmail.com>
2026-05-13 19:19:13 -0500

    Realizé el archivo Register y AuthContext, modificando ligeramente el login y el app.jsx de ZONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import Login from './components/Login';
+import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </AuthProvider>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
